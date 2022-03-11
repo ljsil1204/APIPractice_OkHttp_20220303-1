506791873dff65b285cc2694d50014a7c67daabe
이진실 <ljsil1204@naver.com>
2022-03-11 12:03:14 +0900

    내 좋아요 여부에 따른 텍스트 색상 변경

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.neppplus.apipractice_okhttp_20220303.R
 import com.neppplus.apipractice_okhttp_20220303.ViewTopicDetailActivity
@@ -99,6 +100,16 @@ class ReplyAdapter(
 
         }
 
+//        [도전과제] 싫어요가 눌려도 마찬가지 처리. => 싫어도 API 호출 (기존 함수 활용) + 토론 상세화면 댓글 목록 새로고침
+
+//        좋아요가 눌렸는지, 아닌지. 글씨 색상 변경
+
+        if (data.isMyLike){
+            txtLikeCount.setTextColor(ContextCompat.getColor(mContext, R.color.naver_red))
+        }
+        else{
+            txtLikeCount.setTextColor(ContextCompat.getColor(mContext, R.color.deep_dark_gray))
+        }
 
         return row
 
